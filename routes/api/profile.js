f4872d0574a784249dcfd9c8c2da4877b7c2325b
Sneha Rohra <sneha.rohra99@gmail.com>
2021-12-18 21:58:58 -0500

    Delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -199,5 +199,33 @@ router.put('/experience', [auth,
     }
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    delete experience
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+
+    try{
+        // Profile present or not
+        const profile = await Profile.findOne({ user: req.user.id});
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        // Delete experience from profile
+        profile.experience.splice(removeIndex, 1);
+        
+        // Update database
+        await profile.save();
+
+        // Send success response
+        res.json(profile);
+
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
