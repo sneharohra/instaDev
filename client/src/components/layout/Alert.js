2fcb7f0a8f5527089c50d651cdb5e4853adc438d
Sneha Rohra <sneha.rohra99@gmail.com>
2021-12-20 04:28:30 -0500

    Register and store state

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,13 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 
-const Alert = ({ alerts }) => alerts !== null && alerts.length >0 && alerts.map(alert => (
+const Alert = ({ alerts }) => ( 
     <section className='container'>
-    <div key = { alert.id } className = {`alert alert-${ alert.alertType }`}>
-        { alert.msg }
-    </div>
+    {alerts.map(alert => (
+        <div key = { alert.id } className = {`alert alert-${ alert.alertType }`}>
+            { alert.msg }
+        </div>
+    ))}
     </section>
-));
+);
 
 /*const Alert = ({ alerts }) => (
     <div className="alert-wrapper">
