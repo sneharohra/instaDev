4f5561122f010d362eb8e5c807621b24eb2f27df
Sneha Rohra <sneharohra@Snehas-MacBook-Pro.local>
2021-12-21 19:19:28 -0500

    Setup Post Reducer, Action and Initial Component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,8 @@ const Navbar = ({logout, auth: {isAuthenticated, loading}}) => {
   const authLinks = (
     <ul>
        <li><Link to="/profiles">Developers</Link></li>
+       <li><Link to="/posts">Posts</Link></li>
+
        <li><Link to="/dashboard">
         <i className="fas fa-user" />{' '}
         <span className="hide-sm">Dashboard</span>
